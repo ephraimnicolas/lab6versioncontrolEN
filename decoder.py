5b6c36054f86e318226a8182022278273e31b855
Sneha Nair <nair.sneha@ufl.edu>
2023-11-01 15:16:55 -0400

    Sneha Nair Decoder function

--- decoder.py
+++ decoder.py
@@ -2,9 +2,9 @@
 #Sneha Nair COP3502C
 
 def decode(encoded_password):
-    decode_password = ""
+    decoded_password = ""
     for eachchar in encoded_password:
         newchar = str(int(eachchar) - 3)
-        decode_password += newchar
-    return decode_password
+        decoded_password += newchar
+    return decoded_password
 
