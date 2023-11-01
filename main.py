9d48fc57bc8ad0880b3d63c537667c3e3c36bc4e
Sneha Nair <nair.sneha@ufl.edu>
2023-11-01 15:36:54 -0400

    Sneha Nair Decoder function added

--- main.py
+++ main.py
@@ -12,18 +12,26 @@ def encoder(password):
         new_password += nums
     return new_password
 
+#Sneha Nair COP3502C
+
+def decode(encoded_password):
+    decoded_password = ""
+    for eachchar in encoded_password:
+        newchar = str(int(eachchar) - 3)
+        decoded_password += newchar
+    return decoded_password
+
+
 if __name__ == '__main__':
     while True:
         print_menu()
         choice = int(input("Please enter an option: "))
         if choice == 1:
-            requested_password = input("Please enter your password to 
-encode: ")
+            requested_password = input("Please enter your password to encode: ")
             encoded_password = encoder(requested_password)
             print("Your password has been encoded and stored!")
         elif choice == 2:
-            print(f"The encoded password is {encoded_password}, and the 
-original password is {requested_password}.")
+            print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.")
         elif choice == 3:
             exit()
 
